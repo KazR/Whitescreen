8dd61a5c1b74044eb4e39746c55c8198c7eaf558
Kaz <qasemrana117@gmail.com>
2023-05-19 15:31:33 +0100

    added colors

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# Whitescreen
-Application that turns your screen white. Useful if you need extra lighting in an online meeting,
+# Application Name
+
+A simple tkinter application for changing the background color of a window.
+
+## Description
+
+This application allows users to toggle fullscreen mode and select different background colors for a tkinter window. The available color options include various shades of white and colors associated with lighting.
+
+## Features
+
+- Toggle fullscreen mode
+- Select background color from a dropdown menu
+- Choose from shades of white and lighting-related colors
+
+## Getting Started
+
+### Prerequisites
+
+- Python (version 3.6 or higher)
+- tkinter library (should be included with Python)
+
+### Installation
+
+1. Clone the repository:
+
--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import tkinter as tk
 
-
 # Create the main application window
 window = tk.Tk()
 # Set the window title
@@ -14,12 +13,39 @@ def toggle_fullscreen():
     state = window.attributes('-fullscreen')
     window.attributes('-fullscreen', not state)
 
+def change_background_color(color):
+    window.configure(bg=color)
+
 FullscreenButton = tk.Button(window, text="Toggle Fullscreen", command=toggle_fullscreen)
+FullscreenButton.pack(side="bottom", padx=10, pady=10)
 
-# Position the button at the bottom right
+# Create the color selection dropdown menu
+color_dropdown = tk.StringVar(window)
+color_dropdown.set("White")  # Set the default color
 
-def packs():
-    FullscreenButton.pack(side="bottom", padx=10, pady=10)
+def on_color_selected(*args):
+    selected_color = color_dropdown.get()
+    if selected_color == "Natural Light":
+        change_background_color("#FAF9F6")  # Light creamy white
+    elif selected_color == "Soft Glow":
+        change_background_color("#FDF7E3")  # Soft yellowish white
+    elif selected_color == "Moonlight":
+        change_background_color("#F2F3F4")  # Light grayish white
+    elif selected_color == "Daybreak":
+        change_background_color("#F5F5F5")  # Bright white
+    elif selected_color == "Pure White":
+        change_background_color("white")
+    elif selected_color == "Ivory":
+        change_background_color("#FFFFF0")  # Pale yellowish white
+    elif selected_color == "Snow":
+        change_background_color("#FFFAFA")  # White with a hint of pink
+    elif selected_color == "Pearl":
+        change_background_color("#FDEEF4")  # Pinkish white
+
+color_options = ["White", "Natural Light", "Soft Glow", "Moonlight", "Daybreak", "Pure White", "Ivory", "Snow", "Pearl"]
+color_dropdown_menu = tk.OptionMenu(window, color_dropdown, *color_options, command=on_color_selected)
+color_dropdown_menu.config(direction="right")  # Set the dropdown menu to open on the right
+color_dropdown_menu.pack(side="bottom", padx=10, pady=10)
+
 # Run the application main loop
-packs()
 window.mainloop()
